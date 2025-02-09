2639b9fd051b9d473110545164480f5432ee1de5
SNimsara <nimsaragunasekara123@gmail.com>
2025-02-09 18:21:35 +0530

    create home page

--- glos-website/src/Components/Home/Home.jsx
+++ glos-website/src/Components/Home/Home.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './Home.css'
+import bghome from '../../assets/bghome.png'
 
 const Home = () => {
   return (
     <div className='home'>
-        
+       <div style={{
+            backgroundImage: `url(${bghome})`,
+            backgroundSize: "cover",
+            backgroundRepeat: 'no-repeat',
+            height: "625px",
+            width: "100%",
+        }}>
+            <h1 className='line1'>Where Luxury Meets Radiance,</h1>
+            <h1 className='line2'>Every Piece a Masterpiece.</h1>
+
+            <div className="buttons">
+                <button className='button'>Shop Now</button>
+            </div>
+        </div> 
     </div>
   )
 }
